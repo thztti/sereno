8bc248584690aff09906aeccaf13e25cc0708a1d
Rafael Thomazetti <thztti@gmail.com>
2018-01-04 18:28:58 -0200

    Some refactor was due

--- dev/popup.js
+++ dev/popup.js
@@ -1,68 +1,51 @@
-
-
-chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
+chrome.tabs.onUpdated.addListener( function ( tabId, changeInfo, tab ) {
 	sites = {
-		0:
-		{
+		0: {
 			"domain": "globo.com",
 			"selector": "#boxComentarios"
 		},
-		1:
-		{
+		1: {
 			"domain": "abril.com.br",
 			"selector": "#comments"
 		},
-		2:
-		{
+		2: {
 			"domain": "otempo.com.br",
 			"selector": "#comments"
 		},
-		4:
-		{
+		4: {
 			"domain": "uol.com.br",
 			"selector": "#comentarios"
 		},
-		3:
-		{
+		3: {
 			"domain": "folha.uol",
 			"selector": "#article-comments"
 		},
-		5:
-		{
+		5: {
 			"domain": "bbc.com",
 			"selector": "#story-footer"
 		},
-		5:
-		{
+		6: {
 			"domain": "terra.com.br",
 			"selector": "#fb-social-comments"
 		}
 	}
-  console.log(sites);
-  
 
-  	// const hideComments = y =>
-  		
-  		//switch to arrow functions :/
-	    function hideComments(y){
-	    	var x = "document.querySelector('"+y+"').style.display = 'none'";
-	  		chrome.tabs.executeScript({
-		      code: x
-		    });
-	    }
+	const hideComments = domain => {
+		let script = "document.querySelector('" + domain + "').style.display = 'none'";
+		chrome.tabs.executeScript({
+			code: script
+		});
+	}
 
-  	const urlValidator = (i, tab, sites) =>
-  		// console.log(tab);
-  		tab.url.includes(sites[i]["domain"]) ? hideComments(sites[i]["selector"]) : console.log(19494)
+	const urlValidator = (i, tab, sites) => {
+		let site = sites[i]
 
-  for (var i = 0; i < Object.keys(sites).length; i++) {
-  	console.log(i);
-  	console.log(sites[i]["domain"]);
-  	// if (tab.url.includes(sites[i]["domain"])) {
-  	// 	console.log("navegando em " + sites[i]["domain"]);
-  	// } else {
-  	// 	console.log("oops");
-  	// }
-  	urlValidator(i, tab, sites);
-  }
+			tab.url.includes(site["domain"]) 
+				? hideComments(site["selector"]) 
+				: null
+	}
+
+	for (var i = 0; i < Object.keys(sites).length; i++) {
+		urlValidator(i, tab, sites);
+	}
 });
