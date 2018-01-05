d65104b9128accb387dbd250b5a61ec2968e2f80
Rafael Thomazetti <thztti@gmail.com>
2018-01-05 17:25:18 -0200

    Some refactor

--- dev/popup.js
+++ dev/popup.js
@@ -42,13 +42,8 @@ chrome.tabs.onUpdated.addListener( function ( tabId, changeInfo, tab ) {
 		}
 	}
 
-	
-
 	const hideComments = selector => {
-		// let script = "document.querySelector('" + domain + "').style.display = 'none'";
-		// let script = "document.querySelector('" + selector + "').innerHTML = '" + quote + "'";
 		let script = "var select = document.querySelector('" + selector + "');var quote = document.createElement('div');quote.innerHTML = '<div class=\"sereno sr-container-quote\"><style scoped>.sr-container-quote{margin:20px 0;color:black}.sr-element-paragraph-quote{font-size:20px;width:680px;font-family:Georgia;line-height:25px;overflow:hidden;display:inline-block;padding-left:20px;border-left:5px solid black}.sr-element-footer{display:block;font-size:12px;font-family:Arial;margin-left:30px}a{color:black}</style><span class=\"sr-element-paragraph-quote\">\"Agir, eis a inteligência verdadeira. Serei o que quiser. Mas tenho que querer o que for. O êxito está em ter êxito, e não em ter condições de êxito. Condições de palácio tem qualquer terra larga, mas onde estará o palácio se não o fizerem ali?\" – Roberto Shinyashiki</span><span class=\"sr-element-footer\">Apresentado por <a href=\"http://sereno.news\" target=\"_blank\">Sereno</a></span></div>';select.parentNode.replaceChild(quote, select);";
-		console.log(script)
 		chrome.tabs.executeScript({
 			code: script
 		});
@@ -57,9 +52,9 @@ chrome.tabs.onUpdated.addListener( function ( tabId, changeInfo, tab ) {
 	const urlValidator = (i, tab, sites) => {
 		let site = sites[i]
 
-			tab.url.includes(site["domain"]) 
-				? hideComments(site["selector"]) 
-				: null
+		tab.url.includes(site["domain"]) 
+			? hideComments(site["selector"]) 
+			: null
 	}
 
 	for (var i = 0; i < Object.keys(sites).length; i++) {
