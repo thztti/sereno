03591f8608cc03c85909715b999ab449be47af16
Rafael Thomazetti <thztti@gmail.com>
2018-01-06 10:02:13 -0200

    Shrinks sites object

--- dev/popup.js
+++ dev/popup.js
@@ -1,45 +1,15 @@
 chrome.tabs.onUpdated.addListener( function ( tabId, changeInfo, tab ) {
 	const sites = {
-		0: {
-			"domain": "globo.com",
-			"selector": "#boxComentarios"
-		},
-		1: {
-			"domain": "noticias.band",
-			"selector": "#disqus_thread"
-		},
-		2: {
-			"domain": "abril.com.br",
-			"selector": ".comments"
-		},
-		3: {
-			"domain": "otempo.com.br",
-			"selector": "#comments"
-		},
-		4: {
-			"domain": "redetv.uol.com.br",
-			"selector": ".comentarios"
-		},
-		5: {
-			"domain": "uol.com.br",
-			"selector": "#comentarios, .comments"
-		},
-		6: {
-			"domain": "folha.uol",
-			"selector": "#article-comments"
-		},
-		7: {
-			"domain": "bbc.com",
-			"selector": "#story-footer"
-		},
-		8: {
-			"domain": "terra.com.br",
-			"selector": "#fb-social-comments"
-		},
-		9: {
-			"domain": "estadao.com.br",
-			"selector": ".comentarios"
-		}
+		0: {"domain": "globo.com", 			"selector": "#boxComentarios"},
+		1: {"domain": "noticias.band", 		"selector": "#disqus_thread"},
+		2: {"domain": "abril.com.br", 		"selector": ".comments"},
+		3: {"domain": "otempo.com.br", 		"selector": "#comments"},
+		4: {"domain": "redetv.uol.com.br", 	"selector": ".comentarios"},
+		5: {"domain": "uol.com.br", 		"selector": "#comentarios, .comments"},
+		6: {"domain": "folha.uol", 			"selector": "#article-comments"},
+		7: {"domain": "bbc.com", 			"selector": "#story-footer"},
+		8: {"domain": "terra.com.br", 		"selector": "#fb-social-comments"},
+		9: {"domain": "estadao.com.br", 	"selector": ".comentarios"}
 	}
 
 	const hideComments = selector => {
